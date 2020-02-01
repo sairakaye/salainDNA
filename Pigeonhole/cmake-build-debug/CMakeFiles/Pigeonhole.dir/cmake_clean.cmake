1f6e06524a848a69c8bc6e9e44f0f0fc4d8a4d80
sairakaye <skcmanalili@gmail.com>
2020-02-01 10:24:09 +0800

    Add cmake files/

---
+++ Pigeonhole/cmake-build-debug/CMakeFiles/Pigeonhole.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pigeonhole.dir/addressing.cpp.o"
+  "CMakeFiles/Pigeonhole.dir/common.cpp.o"
+  "CMakeFiles/Pigeonhole.dir/main.cpp.o"
+  "CMakeFiles/Pigeonhole.dir/minimizers.cpp.o"
+  "CMakeFiles/Pigeonhole.dir/pigeonhole.cpp.o"
+  "Pigeonhole"
+  "Pigeonhole.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pigeonhole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
