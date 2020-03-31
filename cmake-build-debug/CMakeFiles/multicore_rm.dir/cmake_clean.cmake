2ebbc29f9dac3132f6c5b702b116de4cb9fd5a6c
sairakaye <skcmanalili@gmail.com>
2020-03-31 13:28:56 +0800

    Initialize files for the over-all integration.

---
+++ cmake-build-debug/CMakeFiles/multicore_rm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multicore_rm.dir/main.cpp.o"
+  "multicore_rm"
+  "multicore_rm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multicore_rm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/multicore_rm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for multicore_rm.
+# This may be replaced when dependencies are built.
