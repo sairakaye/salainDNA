a86afe5f6ada13d5b97c7bd3780d7fb6beb60102
sairakaye <skcmanalili@gmail.com>
2020-04-17 03:49:40 +0800

    Added output when using commands.

--- command.cpp
+++ command.cpp
@@ -5,14 +5,13 @@
 #include "command.h"
 
 string getFileName(string filePath) {
+    char separator = '/';
 
-    char sep = '/';
+    #ifdef _WIN32
+        separator = '\\';
+    #endif
 
-#ifdef _WIN32
-    sep = '\\';
-#endif
-
-    unsigned int i = filePath.rfind(sep, filePath.length());
+    unsigned int i = filePath.rfind(separator, filePath.length());
 
     if (i != string::npos) {
         return (filePath.substr(i+1, filePath.length() - i));
@@ -21,14 +20,39 @@ string getFileName(string filePath) {
     return ("");
 }
 
+void generalDetails() {
+    cout << "A Pre-alignment Filter for DNA Read Mapping on a Multi-core Environment" << endl << endl;
+
+    cout << "The program contains:" << endl;
+    cout << "\t - Indexing (Direct Addressing, Open Addressing, Minimizers)" << endl;
+    cout << "\t - Seed Selector using Pigeonhole" << endl;
+    cout << "\t - Bit-matrix for further filteration" << endl;
+    cout << "\t - Edlib for Verification" << endl;
+}
+
+void helpCommands() {
+    cout << "Arguments:" << endl;
+    cout << "\t -h - view the commands" << endl;
+    cout << "\t -v - view the version" << endl;
+    cout << "\t -q - set the q-gram for the seed and the indexing (default is 8)" << endl;
+    cout << "\t -g - file path of the genome file" << endl;
+    cout << "\t -ir - file path of the input reads for mapping" << endl;
+    cout << "\t -i - file path for the indexing file" << endl;
+    cout << "\t -m - mode of indexing (dir - direct addressing, open - open addressing, min - minimizers)" << endl;
+    cout << "\t -e - error threshold (default is 0)" << endl;
+    cout << "\t -s - search mode for seed selector (all - find locations in each seed, exit - when a location is found in the seed, immediately exit" << endl;
+}
+
 void processingArguments(int argc, char *argv[], string &genomeFilePath, string &readsFilePath, string &indexFilePath, string &mainName) {
     if (argc == 1) {
-        cout << "Print the details of the program." << endl;
+        generalDetails();
+        cout << endl;
+        helpCommands();
         exit(EXIT_SUCCESS);
     } else {
         for (int i = 1; i < argc; ++i) {
             if (string(argv[i]) == "-h") {
-                cout << "Help commands" << endl;
+                helpCommands();
                 exit(EXIT_SUCCESS);
             } else if (string(argv[i]) == "-v") {
                 cout << "Version 1.0.0" << endl;
