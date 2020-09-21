29177e41535a6ff89cb65e9c5f9c70c71e43cb3d
sairakaye <skcmanalili@gmail.com>
2020-09-21 16:28:03 +0800

    Created separate file for verification.

---
+++ verification.h
@@ -0,0 +1,11 @@
+//
+// Created by saimanalili on 21/09/2020.
+//
+
+#ifndef SALAINDNA_VERIFICATION_H
+#define SALAINDNA_VERIFICATION_H
+
+#include "common.h"
+#include "bitmatrix.h"
+
+#endif //SALAINDNA_VERIFICATION_H
